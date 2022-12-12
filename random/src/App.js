04ae67a4104734ec494e8f484ee81916c53df308
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-12-12 21:12:21 +0530

    1

--- random/src/App.js
+++ random/src/App.js
@@ -1,18 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import Todos from "./Components/Todos";
 
 function App() {
-
   return (
     <div className="App">
-     <Todos></Todos>
-   
+      <Todos></Todos>
+      helo
     </div>
   );
 }
 
 export default App;
-    
